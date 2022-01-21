9e90436387c79f9f44656d3d94aa0de73ebf9416
Andrin <88709946+Andrinkaech@users.noreply.github.com>
2022-01-21 14:56:38 +0100

    Test
    .

---
+++ Mond/Mond/Mondreide.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mond
+{
+    internal class Mondreise
+    {
+        double mondreise;
+        double inTagen;
+        double inStunden;
+
+
+    }
+}
